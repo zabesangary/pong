0d41c6b0998778c995f2dfd3c382ef98d86973d1
Zabe <zabivirdee@gmail.com>
2016-10-01 18:31:44 +0200

    Update

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ class Vec
 		this.y = y;
 	}
 }
+
+
 class Rect {
 	constructor(w, h){
 		this.pos = new Vec;
@@ -33,9 +35,35 @@ class Ball extends Rect{
 		this.vel = new Vec;
 	}
 }
+class Pong {
+	constructor(canvas){
+		this._canvas = canvas;
+		this._context = canvas.getContext('2d');
+	}
 
+	update(dt) {
+	ball.pos.x += ball.vel.x * dt;
+ 	ball.pos.y += ball.vel.y * dt;
+
+ 	if (ball.left < 0 ||  ball.right > this._canvas.width) {
+ 		ball.vel.x = -ball.vel.x;
+ 	}
+
+ 	if (ball.top < 0 || ball.bottom > this._canvas.height) {
+ 		ball.vel.y = -ball.vel.y;
+ 	}
+
+	this._context.fillStyle = '#009';
+	this._context.fillRect(0, 0, 
+		this._canvas.width, this._canvas.height);
+
+	this._context.fillStyle = '#fff';
+	this._context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+}
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
+
 
 const ball = new Ball;
 console.log(ball)
